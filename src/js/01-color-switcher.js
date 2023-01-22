28fbdfb0aa83be2e7061e045a441d815b87006f3
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-01-23 00:25:21 +0100

    did 1st hm

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+buttonStart.addEventListener('click', onButtonStartClick);
+buttonStop.addEventListener('click', onButtonStopClick);
+
+let intervalId = null;
+let isActive = false;
+
+function onButtonStartClick() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log(document.body.style.backgroundColor);
+  }, 1000);
+}
+
+function onButtonStopClick() {
+  isActive = false;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
